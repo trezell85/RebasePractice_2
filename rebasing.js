62b1f6b7a1e9c290ad4636395895e82b549feac9
Tarneka Ezell <tarnekaezell@gmail.com>
2021-06-30 19:22:04 -0500

    second commit

--- rebasing.js
+++ rebasing.js
@@ -2,3 +2,8 @@ const alpha = 'alpha';
 const beta = 'beta';
 const gamma = 'gamma';
 const delta = 'delta';
+if (alpha == 1908) {
+  console.log('SkeeWee');
+} else {
+  console.log('Who you with?');
+}
